27e681e7e529d6e306f51d7b23147f9060b8bccd
ZsoltTirkala <tirkala.zsolt@gmail.com>
2020-12-06 09:37:16 +0100

    add slf4j log

--- videoservice/src/main/java/com/codecool/videoservice/service/Videoservice.java
+++ videoservice/src/main/java/com/codecool/videoservice/service/Videoservice.java
@@ -2,12 +2,14 @@ package com.codecool.videoservice.service;
 
 import com.codecool.videoservice.entity.Video;
 import com.codecool.videoservice.repository.VideoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class Videoservice {
 
     @Autowired
