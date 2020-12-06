ab4646c3f2f0342f85c72d705464d44c9f6dee57
ZsoltTirkala <tirkala.zsolt@gmail.com>
2020-12-06 09:37:00 +0100

    add slf4j log and apioperation

--- videoservice/src/main/java/com/codecool/videoservice/controller/VideoController.java
+++ videoservice/src/main/java/com/codecool/videoservice/controller/VideoController.java
@@ -2,6 +2,8 @@ package com.codecool.videoservice.controller;
 
 import com.codecool.videoservice.entity.Video;
 import com.codecool.videoservice.service.Videoservice;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class VideoController {
 
     Videoservice videoService;
@@ -20,6 +23,7 @@ public class VideoController {
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @ApiOperation(value = "Send all the videos")
     @GetMapping("/videos")
     public List<Video> getAllVideo() {
         return videoService.getAllVideo();
